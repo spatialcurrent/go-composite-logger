d1e27b4f168d094b6ee55a6905e90c73691d678d
pjdufour <pjdufour.dev@gmail.com>
2018-04-10 23:36:50 -0400

    Support Fatal messages; NewDefaultLogger()

--- compositelogger/CompositeLogger.go
+++ compositelogger/CompositeLogger.go
@@ -5,6 +5,8 @@ import (
   "os"
   "path/filepath"
   "strings"
+  "io"
+  "io/ioutil"
 )
 
 import (
@@ -15,6 +17,33 @@ import (
 
 type CompositeLogger struct {
   Logs []*logrus.Logger
+}
+
+func OpenOutput(location string) (io.Writer, error) {
+
+  if location == "stdout" {
+    return os.Stdout, nil
+  } else if location == "stderr" {
+    return os.Stderr, nil
+  }
+
+  location_expanded, err := homedir.Expand(location)
+  if err != nil {
+    return ioutil.Discard, errors.New("Error: Could not expand home directory for path " + location + ".")
+  }
+
+  f, err := os.OpenFile(location_expanded, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+  if err != nil {
+    return ioutil.Discard, err
+  }
+
+  filename := filepath.Base(location_expanded)
+
+  if strings.HasSuffix(filename, ".gz") {
+    return gzip.NewWriter(f), nil
+  }
+
+  return f, nil
 }
 
 func NewCompositeLogger(logConfigs []*LogConfig) (*CompositeLogger, error) {
@@ -29,35 +58,15 @@ func NewCompositeLogger(logConfigs []*LogConfig) (*CompositeLogger, error) {
         log.Formatter = &logrus.TextFormatter{}
       }
 
-      if logConfig.Location == "stdout" {
-        log.Out = os.Stdout
-      } else if logConfig.Location == "stderr" {
-        log.Out = os.Stderr
-      } else {
-
-        location, err := homedir.Expand(logConfig.Location)
-        if err != nil {
-          return compositeLogger, errors.New("Error: Could not expand home directory for path " + logConfig.Location + ".")
-        }
-
-        f, err := os.OpenFile(location, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-        if err != nil {
-          return compositeLogger, err
-        }
-
-        filename := filepath.Base(location)
-
-        if strings.HasSuffix(filename, ".gz") {
-          log.Out = gzip.NewWriter(f)
-        } else {
-          log.Out = f
-        }
-
+      out, err := OpenOutput(logConfig.Location)
+      if err != nil {
+        return compositeLogger, err
       }
+      log.Out = out
 
       logLevel, err := logrus.ParseLevel(logConfig.Level)
       if err != nil {
-        return compositeLogger, nil
+        return compositeLogger, err
       }
       log.SetLevel(logLevel)
 
@@ -65,6 +74,43 @@ func NewCompositeLogger(logConfigs []*LogConfig) (*CompositeLogger, error) {
     }
     compositeLogger.Logs = logs
     return compositeLogger, nil
+}
+
+func NewDefaultLogger()  (*CompositeLogger, error)  {
+  logConfigs := []*LogConfig{
+    &LogConfig{
+      Location: "stderr",
+      Level: "warning",
+      Format: "text",
+    },
+  }
+  return NewCompositeLogger(logConfigs)
+}
+
+func (l *CompositeLogger) Add(location string, format string, level string) error {
+
+  log := logrus.New()
+
+  if format == "json" {
+    log.Formatter = &logrus.JSONFormatter{}
+  } else {
+    log.Formatter = &logrus.TextFormatter{}
+  }
+
+  out, err := OpenOutput(location)
+  if err != nil {
+    return err
+  }
+  log.Out = out
+
+  logLevel, err := logrus.ParseLevel(level)
+  if err != nil {
+    return err
+  }
+  log.SetLevel(logLevel)
+
+  l.Logs = append(l.Logs, log)
+  return nil
 }
 
 func (l *CompositeLogger) Info(msg interface{}) {
@@ -77,6 +123,10 @@ func (l *CompositeLogger) Warn(msg interface{}) {
   for _, log := range l.Logs {
     log.Warn(msg)
   }
+}
+
+func (l *CompositeLogger) Fatal(msg interface{}) {
+  l.Logs[0].Fatal(msg)
 }
 
 func (l *CompositeLogger) InfoWithFields(msg interface{}, fields map[string]interface{}) {
